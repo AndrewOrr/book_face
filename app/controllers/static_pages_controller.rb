1660bf126652ed9daf3a5a3394a231f44af09ca2
Olivia Abrant <olivia.abrant@colorado.edu>
2014-04-06 15:21:29 -0600

    added comments to Gemfile and static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home
+  def home		# this is the controller for the home page
   end
 
-  def help
+  def help		# this is the controller for the help page
   end
+
+# add controllers for profile page, news feed page, friends page, etc.
+
 end
