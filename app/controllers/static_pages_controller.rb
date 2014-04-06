9aadbb2f6f8c234b253661b508163d4ae24278ff
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2014-04-05 18:06:50 -0600

    Added comments to Static_Pages_Controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,12 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  #Pages we still need to make
+  #Profile
+  #Profile Preferences
+  #Login
+  #News Feed
+  #Books (If we get there)
+  #Search
 end
