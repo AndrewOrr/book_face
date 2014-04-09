fbf9d100cf6f37587a3fe5a79095ca07245a1696
Andrew Orr <andrew.d.orr@colorado.edu>
2014-04-08 22:40:52 -0600

    adding looks with new branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
-#There's nothing in here yet. I wonder if we'll need it
-
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,23 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
+   describe "Home page" do
 
-    before { visit '/static_pages/home'}
-    it { should have_content('Home') }
-    it { should have_link('Help', href: '/static_pages/Help')}
-end
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  end
 
   describe "Help page" do
 
