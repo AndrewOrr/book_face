c10c450cc32b25a3bb2c947dd0cdd7a0537486a6
Andrew Orr <andrew.d.orr@colorado.edu>
2014-04-21 12:10:27 -0600

    updating stuff

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,8 +9,6 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
-      before { click_button "Sign in" }
-
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
 
@@ -27,16 +25,19 @@ describe "Authentication" do
         click_button "Sign in"
       end
 
+      describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
-  end
+
     describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
-      end
-    end
-  end
-end
+         end
+       end
+     end
