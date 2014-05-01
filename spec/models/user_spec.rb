b59933867f912a52907dab2f8bab020f68fa446d
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2014-05-01 01:38:56 -0600

    Finish user index and destroy actions / cosmetic fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,6 +93,21 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+
+    it {should respond_to(:authenticate) }
+    it {should respond_to(:admin) }
+
+    it {should be_valid}
+    it {should_not be_admin }
+
+    describe "with admin attribute set to 'true'" do
+      before do
+        @user.save!
+        @user.toggle!(:admin)
+      end
+
+      it { should be_admin }
+    end
   end
 
   describe "remember token" do
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,10 +3,28 @@ require 'spec_helper'
 describe "Authentication" do
 
   subject { page }
-describe "authorization" do
+  describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign In') }
+        end
+
+        describe "submitting to the update action" do
+          before {patch user_path(user) }
+          specify { expect(response). to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+      end
 
       describe "when attempting to visit a protected page" do
         before do
@@ -23,19 +41,31 @@ describe "authorization" do
           end
           describe "visiting the user index" do
             before { visit users_path }
-            it { should have_title('Sign in') }
+            it { should have_title('Sign In') }
           end
         end
       end
     end
 
- describe "signin" do
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
 
-    before { visit signin_path }
+      before { sign_in non_admin, no_capybara: true }
 
-    describe "with invalid information" do
-      it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "signin" do
+
+      before { visit signin_path }
+
+      describe "with invalid information" do
+        it { should have_title('Sign in') }
+        it { should have_selector('div.alert.alert-error') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -51,11 +81,11 @@ describe "authorization" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign Out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+      it { should_not have_link('Sign In', href: signin_path) }
     end
     describe "followed by signout" do
       before { click_link "Sign Out" }
-      it { should have_link('Sign in') }
+      it { should have_link('Sign In') }
     end
   end
 end
