fcb243afa5dc88f7fd94646057d0e4fb847937a5
Olivia Abrant <olivia.abrant@colorado.edu>
2014-05-04 01:18:43 -0600

    changed Sign Up! text in static_pages_test.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
     it { should have_content('BookFace') }
-    it { should have_link('Sign up now!', href: signup_path) }
+    it { should have_link('Sign up!', href: signup_path) }
     it { should have_link('Sign In', href: signin_path) }
 
 	describe "for signed-in users" do
